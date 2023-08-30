7dad9092884b753afd65eb07b049fe3ccefd3154
cynthiacodes <cjnkstudy01@hotmail.com>
2023-08-30 15:14:07 +0100

    updated insert queries

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -4,16 +4,28 @@ create table pastes
 (id serial primary key,
  title varchar(55),
  description text not null,
- creation_date date
+ creation_date timestamp default current_timestamp
 );
 
-insert into pastes(title, description,creation_date)
+insert into pastes (title, description)
 values
-('purchase test', 'window.dataLayer = window.dataLayer || [];
-window.dataLayer.push);', '2023-08-29'),
-('html data', '<div class="container">
-    <div class="card border-0" style="background-color: #C5D9F2;">
-        
-        <!-- Header background image (change the link inside the brackets to change the header background) -->
-        <div class="card-header p-3 border-0 justify-content-center"
-            style="background-image: url(https://media.discordapp.net/attachments/1145603288885112852/1146047336796143706/image.png?width=662&height=662);">', '2023-08-23');
+    ('JavaScript Basics', 'console.log("Hello, World!");'),
+    ('Number Multiplication', 'const num = 5;\nconsole.log(num * 2);'),
+    ('Greeting Function', 'function greet(name) {\n  return "Hello, " + name;\n}'),
+    ('Array Length', 'const arr = [1, 2, 3];\nconsole.log(arr.length);'),
+    ('Looping Example', 'for (let i = 0; i < 5; i++) {\n  console.log(i);\n}'),
+    ('Object Properties', 'const person = { name: "Alice", age: 30 };\nconsole.log(person.name);'),
+    ('Conditional Check', 'const x = 10;\nif (x > 5) {\n  console.log("x is greater than 5");\n}'),
+    ('Simple Greeting', 'const greet = () => {\n  console.log("Hello!");\n};'),
+    ('Rectangle Class', 'class Rectangle {\n  constructor(width, height) {\n    this.width = width;\n    this.height = height;\n  }\n}'),
+    ('Color Iteration', 'const colors = ["red", "green", "blue"];\nfor (const color of colors) {\n  console.log(color);\n}'),
+    ('Factorial Function', 'const factorial = (n) => {\n  if (n <= 1) return 1;\n  return n * factorial(n - 1);\n};'),
+    ('Even Number Check', 'const isEven = num => num % 2 === 0;\nconsole.log(isEven(4));'),
+    ('Promise Example', 'const promise = new Promise((resolve, reject) => {\n  setTimeout(() => resolve("Done"), 1000);\n});'),
+    ('Personalized Greeting', 'const greet = name => `Hello, ${name}!`;\nconsole.log(greet("Bob"));'),
+    ('Array Doubling', 'const numbers = [1, 2, 3, 4, 5];\nconst doubled = numbers.map(num => num * 2);\nconsole.log(doubled);');
+    
+
+select * from pastes where id = 1;
+
+select * from pastes order by creation_date desc limit 10;
